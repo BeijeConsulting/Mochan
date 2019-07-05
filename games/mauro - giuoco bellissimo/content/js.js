ecb0d6820f92390d52a5c9204cd30400960eea06
BlancoTarrendi <tentimauro@gmail.com>
2019-07-05 12:53:29 +0200

    Sfondo di gioco

--- games/mauro - giuoco bellissimo/content/js.js
+++ games/mauro - giuoco bellissimo/content/js.js
@@ -1,1 +1,24 @@
 console.log("questo è un bel gioco davvero")
+
+var playArea = {
+    height: document.getElementById("play_area").clientHeight,
+    width: document.getElementById("play_area").clientWidth
+}
+
+setInterval(function () {clouds()}, 2500);
+
+function clouds() {
+    let cloud = document.createElement("img");
+    let rndWidth = randomizer(1000);
+    cloud.src = "./imgs/cloud.png";
+    cloud.width = rndWidth;
+    cloud.style.position = "absolute";
+    cloud.style.right = "-" + rndWidth + "px";
+    cloud.style.top = randomizer(playArea.height) + "px";
+    document.getElementById("play_area").appendChild(cloud);
+}
+
+function  randomizer(param) {
+    let randomized = Math.floor(Math.random() * param);
+    return randomized;
+}
