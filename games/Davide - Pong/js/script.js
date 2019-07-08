c0b590f504968dd71c896674f6d4726532576dff
Davide <davide.prete1994@gmail.com>
2019-07-08 08:52:31 +0200

    Aggiunto punteggio e bugfix rimbalzo pallina

--- games/Davide - Pong/js/script.js
+++ games/Davide - Pong/js/script.js
@@ -2,15 +2,19 @@ var lineTop = document.getElementById("lineTop");
 var lineBot = document.getElementById("lineBot");
 var ball = document.getElementById("ball");
 var box = document.getElementById("box-game");
-var pos = 100;
-var posB = 200;
+var boxWidth = box.clientWidth;
+var pos = (boxWidth-70)/2;
+var posB = 290;
+var posBX = (boxWidth-20)/2;
 var vertical = 0;
-var posBX = 115;
+var rand= 0;
+var scoreA=0;
+var scoreB=0;
+var animation;
 lineTop.style.left = pos + 'px';
 lineBot.style.left = pos + 'px';
 ball.style.top = posB + 'px';
 ball.style.left = posBX + 'px';
-
 document.onkeydown = checkKey;
 function checkKey(e) {
     e = e || window.event;
@@ -26,71 +30,122 @@ function checkKey(e) {
         lineBot.style.left = pos + 'px';
         
     }
-    else if (e.keyCode == '39' && pos<200) {
+    else if (e.keyCode == '39' && pos<boxWidth-70) {
         pos=pos+10;
         lineTop.style.left = pos + 'px'; // right arrow
         lineBot.style.left = pos + 'px';
         
     }
 }
-var rand= 0;
-var animation = setInterval(draw,15);
 function draw(){
     ball.style.top = posB + 'px';
     ball.style.left = posBX + 'px';
-    posBX = posBX + rand;
-    console.log(posBX);
-    if(posBX<1||posBX>230){
+    if(posBX<1||posBX>(boxWidth-20)){
         rand=rand*(-1);
     }
     if(vertical==0){
         posB = posB-3;
-        if (isCollapsed(ball, lineTop)){
+        if (isCollapsed(ball, lineTop)==1){
             vertical=1;
-            rand= Math.floor(Math.random() * 5) -2;
+            if(rand<0){
+                rand++;
+            }else{
+                rand--;
+            }
+            rand=rand*(-1);
+        }else if (isCollapsed(ball, lineTop)==2){
+            vertical=1;
+            rand=-3;
+        }else if (isCollapsed(ball, lineTop)==3){
+            vertical=1;
+            rand=3;
         }
         else {
-            if(posB<-30){
+            if(posB<-20){
                 clearInterval(animation);
-                document.getElementById("restart").style.display="block"; 
+                document.getElementById("overlay").style.display="block";
+                setTimeout(function(){goal()}, 3000);
+                scoreB++;
+                document.getElementById("scoreB").innerHTML=scoreB;
             }
             //ball.style.display="none";
             //clearInterval(animation);
         }
     }else{
         posB = posB+3;
-        if (isCollapsed(ball, lineBot)){
+        if (isCollapsed(ball, lineBot)==1){
             vertical=0;
-            rand= Math.floor(Math.random() * 5) + -2;
+            if(rand<0){
+                rand++;
+            }else{
+                rand--;
+            }
+            rand=rand*(-1);
+        }
+        else if (isCollapsed(ball, lineBot)==2){
+            vertical=0;
+            rand=-3;
+        }else if (isCollapsed(ball, lineBot)==3){
+            vertical=0;
+            rand=3;
         }
         else{
-            if(posB>430){
+            if(posB>620){
                 clearInterval(animation);
-                document.getElementById("restart").style.display="block";  
+                document.getElementById("overlay").style.display="block";
+                setTimeout(function(){goal()}, 3000);
+                scoreA++;
+                document.getElementById("scoreA").innerHTML=scoreA;
             }
             //ball.style.display="none";
             //clearInterval(animation);
         }
     }
+    posBX = posBX + rand;
 }
 
 function isCollapsed(dragMe, rect){
+    var result=0;
     var object_1 = dragMe.getBoundingClientRect();
     var object_2 = rect.getBoundingClientRect();
     
     if (object_1.left < object_2.left + object_2.width  && object_1.left + object_1.width  > object_2.left &&
           object_1.top < object_2.top + object_2.height && object_1.top + object_1.height > object_2.top) {
-      return true;
+            if (object_2.left - object_1.left >0 ) {
+                result = 2;
+            }else if (object_2.left - object_1.left <-34 ) {
+                result = 3;
+            }else{
+                result =1;
+            }
+            
     }
     else{
-      return false;
+        result = 0;
     }
+    return result;
 }
+var oldX;
+function move(e) {
+    var x = e.clientX;
+    if (oldX<x&&pos<boxWidth-70){
+        pos=pos+3;
+        lineTop.style.left = pos + 'px'; // right arrow
+        lineBot.style.left = pos + 'px';
+    }else if(pos>0){
+        pos=pos-3;
+        lineTop.style.left = pos + 'px'; // left arrow
+        lineBot.style.left = pos + 'px';
+    }
+    oldX=x;
+}  
+
 function restart(){
     document.getElementById("restart").style.display="none";  
-    posB=200;
-    posBX=115;
-    pos = 100;
+    document.getElementById("overlay").style.display="none";  
+    posB=290;
+    posBX=(boxWidth-20)/2;
+    pos = (boxWidth-70)/2;
     lineTop.style.left = pos + 'px';
     lineBot.style.left = pos + 'px';
     ball.style.top = posB + 'px';
@@ -98,6 +153,15 @@ function restart(){
     animation = setInterval(draw,15);
     rand= 0;
 }
-function changeDir(){
-
+function goal(){
+    document.getElementById("overlay").style.display="none";  
+    posB=290;
+    posBX=(boxWidth-20)/2;
+    pos = (boxWidth-70)/2;
+    lineTop.style.left = pos + 'px';
+    lineBot.style.left = pos + 'px';
+    ball.style.top = posB + 'px';
+    ball.style.left = posBX + 'px';
+    animation = setInterval(draw,15);
+    rand= 0;
 }
