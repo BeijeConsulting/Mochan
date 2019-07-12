33ce6d8ab67010a38fd2819a7e82592b4c56cd68
Davide <davide.prete1994@gmail.com>
2019-07-12 17:25:36 +0200

    Aggiunta visualizzazione commenti ai post

--- React/Davide/utenti/src/components/Post.js
+++ React/Davide/utenti/src/components/Post.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Comments from "./Comments.js";
 class Posts extends Component {
     render() {
         return (
                 <div className="posts-container">
                     <div className="post-title">{this.props.title}</div>
                     <p>{this.props.text}</p>
+                    <Comments postId={this.props.postId} />
                 </div>
         );
     }
