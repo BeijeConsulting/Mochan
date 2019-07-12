a152009078e190c104824288a9372eda8c7934ef
Davide <davide.prete1994@gmail.com>
2019-07-12 15:27:45 +0200

    Aggiunta completa app social

--- React/Davide/utenti/src/App.js
+++ React/Davide/utenti/src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route} from "react-router-dom";
+import HomeView from "./components/HomeView.js";
+import UserView from "./components/UserView.js";
+import TopBar from "./components/TopBar.js";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter> 
+        <header className="App-header">
+          <TopBar />
+        </header>
+        <Route exact path="/" component={HomeView} />
+        <Route exact path="/users/" component={UserView} />
+      </BrowserRouter>
     </div>
   );
 }
